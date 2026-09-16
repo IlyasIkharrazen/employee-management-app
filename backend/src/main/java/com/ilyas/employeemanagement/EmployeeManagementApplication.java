32fe286cbb7b7951f72563bc2dffe0e8fc16005c
Ilyas Ikharrazen <valenciennesilyas@hotmail.com>
2026-09-16 17:13:16 +0200

    ok

--- backend/src/main/java/com/ilyas/employeemanagement/EmployeeManagementApplication.java
+++ backend/src/main/java/com/ilyas/employeemanagement/EmployeeManagementApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
+		 SpringApplication.run(EmployeeManagementApplication.class, args);
+		 
 	}
 
 }

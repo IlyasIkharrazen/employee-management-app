a0352ccb5c62a9a173f303f6e697bae8e29a9846
Ilyas Ikharrazen <valenciennesilyas@hotmail.com>
2026-09-22 20:37:19 +0200

    ajout component employee list

---
+++ frontend/src/app/features/employees/pages/employee-list/employee-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-employee-list',
+  styleUrl: './employee-list.scss',
+  templateUrl: './employee-list.html',
+})
+export class EmployeeList {}

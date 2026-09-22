a3d51b32dad16005b6344768a70dca19d5f4095c
Ilyas Ikharrazen <valenciennesilyas@hotmail.com>
2026-09-22 20:43:55 +0200

    ajout de la route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeeList } from './features/employees/pages/employee-list/employee-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'employees',
+        component: EmployeeList
+    }
+];

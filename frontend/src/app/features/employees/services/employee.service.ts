81d35f1aef96018ad00b7f2d0e513d4793786ba0
Ilyas Ikharrazen <valenciennesilyas@hotmail.com>
2026-09-23 01:28:53 +0200

    ajout de la requete get all employees

--- frontend/src/app/features/employees/services/employee.service.ts
+++ frontend/src/app/features/employees/services/employee.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,8 @@ import { Injectable, Service } from '@angular/core';
 export class EmployeeService {
     constructor(private http: HttpClient) {
 
+    }
+    getEmployees(): Observable<Employee[]>{
+        return this.http.get<Employee[]>('http:localhost/8080/employees');
     }
 }

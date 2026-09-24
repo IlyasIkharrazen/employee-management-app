f2c011983cf30ee190044e227576af7cc4ca41c5
Ilyas Ikharrazen <valenciennesilyas@hotmail.com>
2026-09-24 20:24:05 +0200

    app_user

--- backend/src/main/java/com/ilyas/employeemanagement/entity/User.java
+++ backend/src/main/java/com/ilyas/employeemanagement/entity/User.java
@@ -1,19 +1,18 @@
 package com.ilyas.employeemanagement.entity;
 
 import com.ilyas.employeemanagement.enums.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+        
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
